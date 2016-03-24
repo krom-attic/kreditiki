ad6a81692e9ea939dfcc25d33b3bbb304a95280b
CyberZass <cyberzass@gmail.com>
2016-03-24 17:06:24 +0200

    Signed-off-by: CyberZass <cyberzass@gmail.com>

--- kreddb/static/kreddb/js/main.js
+++ kreddb/static/kreddb/js/main.js
@@ -109,12 +109,10 @@ $(document).ready(function(){
         }
     });
 
-    // схлопывает популярные модели на главной для мобильных устройств
-    var wWidth = $(window).width();
-    if(wWidth <= 768) {
-        $('.btn-popular-models').attr('aria-expanded','false');
-        $('#popular-models').removeClass('in');
-    }
+    // убирает скрывающий класс у блока с популярными моделями
+    $('.btn-popular-models').on('click', function() {
+        $('#popular-models').removeClass('hidden-xs');
+    });
 
     // до сих
 
