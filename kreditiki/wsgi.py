11f52cddac29f15984a1a4170791b5df575637b6
Krom <krommail@gmail.com>
2016-07-09 14:49:54 +0300

    manage.py теперь тоже учитывает переменные окружения

--- kreditiki/wsgi.py
+++ kreditiki/wsgi.py
@@ -14,9 +14,9 @@ from django.core.wsgi import get_wsgi_application
 # Дефолтный вариант
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kreditiki.settings")
 
-# Возможно стоит добавить на хероке энву
-if 'DYNO' in os.environ:
-    from dj_static import Cling
-    application = Cling(get_wsgi_application())
-else:
-    application = get_wsgi_application()
+# Этот вариант имеет смысл, если деплоить стэйджинг на хероку
+# if 'DYNO' in os.environ:
+#     from dj_static import Cling
+#     application = Cling(get_wsgi_application())
+# else:
+application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+import json
 import os
 import sys
 
 if __name__ == "__main__":
+    with open('env.json', 'r') as env_file:
+        for key, value in json.loads(env_file.read())['env_vars'].items():
+            os.environ[key] = value
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kreditiki.settings.dev_settings")
 
     from django.core.management import execute_from_command_line
