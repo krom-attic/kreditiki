90f4460d926c2b8fc1c9366924e47bfe8ef84714
Krom <krommail@gmail.com>
2016-10-23 20:47:11 +0300

    фикс заявки

--- kreddb/urls.py
+++ kreddb/urls.py
@@ -25,6 +25,32 @@ URL_RE_PARTS = {
 }
 
 urlpatterns = [
+    url(r'^$', views.CarMakeListView.as_view(), name='list_marks'),
+    url(r'^contact-us/$', views.ContactUsView.as_view(), name='contact_us'),
+
+    url(r'^кредит на/$', views.CarMakeListView.as_view(), name='list_marks'),
+
+    url(r'^selector/$', views.CarSelectorDispatchView.as_view(), name="carselector"),
+
+    url(r'^кредит на/{car_make}/(?P<all>все/)?$'.format(**URL_RE_PARTS), views.CarModelListView.as_view(), name='list_model_families'),
+
+    url(r'^кредит на/{car_make}/{car_model}/{body}/{gen_year_start}/{object_id}/(?P<all>все/)?$'.format(**URL_RE_PARTS),
+        views.ModificationListView.as_view(), name='list_modifications'),
+
+    # TODO можно после gen_year_start передавать ещё несколько годов через запятую
+    url(r'^кредит на/{car_make}/{car_model}/{body}/{gen_year_start}/{generation}/{complect}/{engine}/{gear}/'
+        r'{object_id}/$'.format(**URL_RE_PARTS), views.ModificationDetailView.as_view(),
+        name='view_modification'),
+    url(r'^кредит на/{car_make}/{car_model}/{body}/{gen_year_start}/{generation}/{complect}/{engine}/{gear}/'
+        r'{object_id}$'.format(**URL_RE_PARTS), views.ModificationDetailView.as_view(), name='view_modification'),
+
+    url(r'^заявка/', views.CreditApplicationView.as_view(), name='credit_application'),
+
+    url(r'^service/upload_csv/$', service.UploadCarCsvView.as_view(), name='service_upload_csv'),
+    url(r'^service/csv_template/$', service.DownloadCarCsvTemplate.as_view(), name='service_download_csv_template'),
+    url(r'^service/upload_images/$', service.UploadCarImagesView.as_view(), name='service_upload_images'),
+
+    url(r'^service/set_promo/$', service.SetPromoView.as_view(), name='service_set_promo'),
     # TODO что это?
     url(r'^api/car_makes/{car_make}/car_models/$'.format(**URL_RE_PARTS),
         views.CarModelListAPIView.as_view(),
@@ -34,30 +60,5 @@ urlpatterns = [
         views.ModificationDataApiView.as_view(),
         name='modification_data'),
 
-    # TODO заменить credit на кредит
-    url(r'^$', views.CarMakeListView.as_view(), name='list_marks'),
-    url(r'^contact-us$', views.ContactUsView.as_view(), name='contact_us'),
 
-    url(r'^кредит/$', views.CarMakeListView.as_view(), name='list_marks'),
-
-    url(r'^selector/$', views.CarSelectorDispatchView.as_view(), name="carselector"),
-
-    url(r'^кредит/{car_make}/(?P<all>все/)?$'.format(**URL_RE_PARTS), views.CarModelListView.as_view(), name='list_model_families'),
-
-    url(r'^кредит/{car_make}/{car_model}/{body}/{gen_year_start}/{object_id}/(?P<all>все/)?$'.format(**URL_RE_PARTS),
-        views.ModificationListView.as_view(), name='list_modifications'),
-
-    # TODO можно после gen_year_start передавать ещё несколько годов через запятую
-    url(r'^кредит/{car_make}/{car_model}/{body}/{gen_year_start}/{generation}/{complect}/{engine}/{gear}/'
-        r'{object_id}/$'.format(**URL_RE_PARTS), views.ModificationDetailView.as_view(),
-        name='view_modification'),
-    url(r'^кредит/{car_make}/{car_model}/{body}/{gen_year_start}/{generation}/{complect}/{engine}/{gear}/'
-        r'{object_id}$'.format(**URL_RE_PARTS), views.ModificationDetailView.as_view(), name='view_modification'),
-
-    url(r'^кредит/заявка/', views.CreditApplicationView.as_view(), name='credit_application'),
-
-    url(r'^service/upload_csv/$', service.UploadCarCsvView.as_view(), name='service_upload_csv'),
-    url(r'^service/csv_template/$', service.DownloadCarCsvTemplate.as_view(), name='service_download_csv_template'),
-    url(r'^service/upload_images/$', service.UploadCarImagesView.as_view(), name='service_upload_images'),
-    url(r'^service/set_promo/$', service.SetPromoView.as_view(), name='service_set_promo'),
 ]
