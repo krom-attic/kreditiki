be0dff1272371693d7bd72ba5cfbced7248efbdf
Krom <krommail@gmail.com>
2016-10-29 23:52:59 +0300

    favicon

--- kreditiki/settings/base_settings.py
+++ kreditiki/settings/base_settings.py
@@ -85,10 +85,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kreditiki.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-# Set in env-specific settings
+# Database settings are set in env-specific settings
 
 
 # Internationalization
