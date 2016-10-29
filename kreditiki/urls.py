4d7243c6442ebd64ae975d0449269ba623a412c0
Krom <krommail@gmail.com>
2016-10-29 23:42:20 +0300

    robots

--- kreditiki/urls.py
+++ kreditiki/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^', include('kreddb.urls', namespace='kreddb'))
 ]
 
