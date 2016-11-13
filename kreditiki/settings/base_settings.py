97e337da3aa27a48c4e56d66a44526796a4f478e
Krom <krommail@gmail.com>
2016-11-14 00:06:06 +0300

    подчистка settings, часть 2

--- kreditiki/settings/base_settings.py
+++ kreditiki/settings/base_settings.py
@@ -157,3 +157,82 @@ LOGGING = {
 }
 
 logging.config.dictConfig(LOGGING)
+
+# Настройки, идущие ниже, зависят от среды
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+if SECRET_KEY is None:
+    SECRET_KEY = '9pb4en8!fqh9jtk@iipkk%hc4rb__78aadii))3fj+vqi87t+_'
+
+    DEBUG = os.environ.get('DEBUG') != 'False'
+
+    INTERNAL_IPS = ['127.0.0.1']
+
+    INSTALLED_APPS.append('debug_toolbar')
+
+    # You should include the Debug Toolbar middleware as early as possible in the list. However, it must come after
+    # any other middleware that encodes the response’s content, such as GZipMiddleware.
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+    # Static asset configuration
+    MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    # если удалить, не будут отправляться письма через mail_managers()
+    MANAGERS = [
+        'Dummy manager', 'manager@example.com'
+    ]
+else:
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = os.environ['DEBUG'] == "True"
+
+    # безопасность
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_SSL_REDIRECT = True
+    # настройка определения безопасного соединения, прошедшего через прокси
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+
+    MEDIA_ROOT = os.path.join(os.environ['WWW_ROOT'], os.environ['MEDIA_DIR'])
+
+    EMAIL_HOST = os.environ['EMAIL_HOST']
+    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
+    ADMINS = [
+        (os.environ['ADMIN_NAME'], os.environ['ADMIN_EMAIL'])
+    ]
+
+    MANAGERS = ADMINS + [
+        (os.environ['MANAGER_NAME'], os.environ['MANAGER_EMAIL'])
+    ]
+
+    SERVER_EMAIL = os.environ['SERVER_EMAIL']
+
+    # TODO использовать кэширующий загрузчик шаблонов
+    # https://docs.djangoproject.com/en/1.10/ref/templates/api/#template-loaders
+
+    # TODO использовать постоянное соединение с БД
+    # https://docs.djangoproject.com/en/1.10/ref/databases/#persistent-database-connections
+
+# Allow host headers
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': '3306',
+    }
+}
--- manage.py
+++ manage.py
@@ -3,15 +3,13 @@ import json
 import os
 import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         with open('env.json', 'r') as env_file:
             for key, value in json.loads(env_file.read()).items():
                 os.environ.setdefault(key, value)
     except FileNotFoundError:
-        # TODO It defaults to DEVELOPMENT mode, but shouldn't
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kreditiki.settings.dev_settings")
-        os.environ.setdefault("LOG_ROOT", "")
+        raise Exception('env.json should be present and contain configuration')
 
     from django.core.management import execute_from_command_line
 
